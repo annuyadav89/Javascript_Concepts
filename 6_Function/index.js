eb7f2c9eef24d3f908aefc92e29c90c99c969cd7
annuyadav89 <123576450+annuyadav89@users.noreply.github.com>
2024-04-26 11:06:06 +0000

    done

--- 6_Function/index.js
+++ 6_Function/index.js
@@ -1,7 +1,17 @@
-let sum = function(a,b){
+let sum = function(a,b){ // function expression
     let s = a+b;
    console.log(s);
     
 }
 let yog = sum(3,2);
 console.log(yog);
+
+// ============================================================
+
+
+function hello(){  // function declaration
+    console.log("hello dharam");
+}
+
+hello();
+
