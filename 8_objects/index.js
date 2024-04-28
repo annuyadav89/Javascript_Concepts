849f0b251e617e3e1619f09d1c93347e6ea6381d
annuyadav89 <123576450+annuyadav89@users.noreply.github.com>
2024-04-28 15:02:16 +0000

    code

--- 8_objects/index.js
+++ 8_objects/index.js
@@ -1,15 +1,66 @@
 // let person = {};
 // console.log(typeof(person));
 
-let person = {
-   name:"annu",
-   lastName:"yadav",
-   age:21,
-   hobbies:["cricket","travel","movies"],
-   message:function(){
-     console.log("hello everyone this is "+ this.name);
-   }
-}
+// ==========================================================================================================
 
-// When a function is a property of an object, it is also referred to as a method.
-console.log(person);
+// let person = {
+//    name:"annu",
+//    lastName:"yadav",
+//    age:21,
+//    hobbies:["cricket","travel","movies"],
+//    message:function(){
+//      console.log("hello everyone this is "+ this.name);
+//    }
+// }
+
+// // When a function is a property of an object, it is also referred to as a method.
+// console.log(person);
+
+// ==================================================================================================================
+
+// const person = new Object();
+// console.log(person);
+
+// person.firstName = "dharam";
+// person.lastName = "yadav";
+// person.age = 2;
+// person.toys = ["baby voice","guitar","laptop","snake","cat"]
+// person.playing = function(){
+//   console.log("he is not playing more than 2 days with the same toy");
+// }
+
+// console.log(person);
+// person.toys[5] = "panda"
+// console.log(person);
+
+// ======================================================================================================================
+
+// const person = {
+//   name:"dharam",
+//   age:2,
+//   gender:"boy"
+// }
+// console.log(person);
+
+// const person2 = Object.create(person);
+// person2.age = 5;
+// person2.name = "gullu"
+// console.log(person2);
+
+// ===============================================================================================================================
+
+// function person(){
+//   return{
+//     name:"dharam",
+//     age:2,
+//     gender:"boy"
+//   }
+// }
+
+// console.log(person);
+
+// const person2 = person();
+
+// console.log(person2);
+
+//  ========================================================================================================
